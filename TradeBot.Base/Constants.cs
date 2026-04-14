3578467cd6ffe0c83102545545d9e7b0c0810b9c
Efi4 <sm.a.a.zim914@gmail.com>
2026-04-14 21:21:39 +0000

    Fix env variable name constants

--- TradeBot.Base/Constants.cs
+++ TradeBot.Base/Constants.cs
@@ -2,7 +2,7 @@ namespace TradeBot.Base;
 
 public static class Constants
 {
-    public const string AzureStorageConnectionStringEnvironmentVariableName = "ConnectionStrings:tradingStorageAccount";
-    public const string AzureSqlConnectionStringEnvironmentVariableName = "ConnectionStrings:tradingDatabase";
+    public const string AzureStorageConnectionStringEnvironmentVariableName = "ConnectionStrings__tradingStorageAccount";
+    public const string AzureSqlConnectionStringEnvironmentVariableName = "ConnectionStrings__tradingDatabase";
 
 }
