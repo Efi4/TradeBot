f21d30deece04cfd4aec12a1314ced313de38ac1
Efi4 <sm.a.a.zim914@gmail.com>
2026-04-15 16:44:47 +0000

    Deploy functionapp to test keyvault connectivity

--- AzureFunctionApp/Functions/HttpTriggerFunction.cs
+++ AzureFunctionApp/Functions/HttpTriggerFunction.cs
@@ -2,24 +2,30 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using System.Threading.Tasks;
+using TradeBot.Core.Interfaces;
 
 namespace AzureFunctionApp.Functions
 {
     public class HttpTriggerFunction
     {
         private readonly ILogger<HttpTriggerFunction> _logger;
-
-        public HttpTriggerFunction(ILogger<HttpTriggerFunction> logger)
+        private readonly ICheckTheAvPricesService _avpriceService;
+        public HttpTriggerFunction(
+            ILogger<HttpTriggerFunction> logger, 
+            ICheckTheAvPricesService avpriceService)
         {
             _logger = logger;
+            _avpriceService = avpriceService;
         }
 
         [Function("HttpTriggerFunction")]
-        public HttpResponseData Run(
+        public async Task<HttpResponseData> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "hello")] HttpRequestData req)
         {
             _logger.LogInformation("HTTP trigger function processed a request.");
 
+            var result = await _avpriceService.CheckPricesAsync();
             var response = req.CreateResponse(HttpStatusCode.OK);
             return response;
         }
