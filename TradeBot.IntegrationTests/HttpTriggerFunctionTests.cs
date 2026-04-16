bb30f599a5682fc6a616e388ba76a038c929ce20
Efi4 <sm.a.a.zim914@gmail.com>
2026-04-16 11:00:21 +0000

    Introduce basic integration tests

---
+++ TradeBot.IntegrationTests/HttpTriggerFunctionTests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using RestSharp;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TradeBot.IntegrationTests
+{
+    [TestFixture]
+    public class HttpTriggerFunctionTests
+    {
+        private RestClient _client = null!;
+        private const string FunctionAppBaseUrl = "http://localhost:7071";
+        private const string HelloFunctionRoute = "/api/hello";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _client = new RestClient(FunctionAppBaseUrl);
+        }
+
+        [Test]
+        public async Task HttpTriggerFunction_PostRequest_ShouldReturn200()
+        {
+            // Arrange
+            var request = new RestRequest(HelloFunctionRoute, Method.Post);
+
+            // Act
+            var response = await _client.ExecuteAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.IsSuccessful.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task HttpTriggerFunction_PostRequest_ShouldHaveValidResponse()
+        {
+            // Arrange
+            var request = new RestRequest(HelloFunctionRoute, Method.Post);
+
+            // Act
+            var response = await _client.ExecuteAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Should().NotBeNull();
+        }
+    }
+}
