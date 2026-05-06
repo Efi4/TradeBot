c57b3f58bcbdd9270c3a61af33aaf4723d3b505b
Efi4 <sm.a.a.zim914@gmail.com>
2026-05-06 19:21:20 +0000

    Adjust function cron expression to calculate average price less frequently

--- AzureFunctionApp/Functions/CalculateAveragePrices.cs
+++ AzureFunctionApp/Functions/CalculateAveragePrices.cs
@@ -20,7 +20,7 @@ namespace AzureFunctionApp.Functions
 
         [Function(nameof(CalculateAveragePrices))]
         public async Task Run(
-            [TimerTrigger("30 */11 * * * *")] TimerInfo myTimer)
+            [TimerTrigger("0 0 */2 * * *")] TimerInfo myTimer)
         {
             _logger.LogDebug($"{nameof(CalculateAveragePrices)}: Timer trigger function executed at: {DateTime.Now}");
             
